78bdd6d3bd1a15ffed5c663d0511f8ff036310dc
Gustavo Henrique <guazambujinha@gmail.com>
2025-02-03 17:50:46 -0300

    fist commit

---
+++ Application/Program.java
@@ -0,0 +1,13 @@
+package Application;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner dados = new Scanner(System.in);
+
+
+        System.out.println("Informe a quantidade de números que deseja apostar:");
+        int qtd = dados.nextInt();
+    }
+}
